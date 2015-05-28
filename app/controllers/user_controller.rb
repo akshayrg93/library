802fb84f7416b5961e15e3e414742de35968e6b7
akshayrg93 <akshay@qburst.com>
2015-05-28 17:44:19 +0530

    Users completed... bugs on edit and delete

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,44 @@
 class UserController < ApplicationController
-	def new
-		
+	def index
+		@users = User.all
 	end
-	def create
-		@user = User.new
+	
+ 	def show
+  		@user = User.find(params[:id])
   	end
+
+	def new
+		@user = User.new
+	end
+
+  	def edit
+   	 @user = User.find(params[:id])
+  	end
+
+	def create
+  		@user = User.new(user_params)
+ 		if @user.save
+ 			redirect_to @user
+ 		else
+ 			render 'new'
+ 		end	
+	end
+ 	
+	def update
+  		@user = User.find(params[:id])
+ 		if @user.update(user_params)
+    		redirect_to @user
+  		end
+	end
+	def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+	  redirect_to user_index_path
+    end
+	private
+  	def user_params
+    	params.require(:user).permit(:name, :address, :email, :phone, :state, :country)
+  	end
+  	
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+	validates :name, :address, :email, :phone, :state, :presence => {:message => "empty value found"}
 end
