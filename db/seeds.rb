67831a92c48dff44d9b3b81d382d9f1f1570d490
akshayrg93 <akshay@qburst.com>
2015-05-28 12:50:50 +0530

    Initial Commit

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+country_list = [
+  "Germany" ,
+  "France",
+  "India",
+  "Netherlands",
+  "USA" ,
+  "England" ,
+  "Australia" ,
+  "Spain" ,
+  "Argentina" ,
+  "Italy" ,
+]
+
+country_list.each do |name|
+  Country.create( name: name)
+end
