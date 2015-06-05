0792d88a88b9fee67ef76916cb52f0cf557e5d00
akshayrg93 <akshay@qburst.com>
2015-06-05 16:19:44 +0530

    Completed validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ActiveRecord::Base
-	validates :name, :author, :language, :details, :presence => {:message => "empty value found"}
+	validates :name, :author, :language, :details, presence: true
 	belongs_to :user, class_name: 'User', foreign_key: :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
-	validates :name, :address, :email, :phone, :state, :presence => {:message => "empty value found"}
+	validates :name, presence: true, length: { minimum: 2 }
+	validates :address, presence: true	
+	validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+	validates :phone, presence: true, numericality: { only_integer: true }, length: { is: 10 }	
+	validates :state, presence: true	
 	has_many:book, :class_name => 'Book', :foreign_key => 'user_id'
 end
