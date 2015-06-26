9ac8b0899b7de0c0f7fb2ec49ec0fc493048a3a8
akshayrg93 <akshay@qburst.com>
2015-06-26 12:36:44 +0530

    Fixed error in migrations --

--- db/migrate/20150528051258_create_users.rb
+++ db/migrate/20150528051258_create_users.rb
@@ -10,5 +10,14 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+    create_table :books do |t|
+      t.belongs_to :user, index: true
+      t.string :name
+      t.string :author
+      t.string :language
+      t.string :details
+      t.string :user_id
+      t.timestamps null: false
+    end
   end
 end
