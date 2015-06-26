4be1b85973b26fffb0d86c0755160621237366e0
akshayrg93 <akshay@qburst.com>
2015-06-26 13:32:47 +0530

    Fixed error with password confirmation

--- lib/seeds/admin_info.rb
+++ lib/seeds/admin_info.rb
@@ -1,5 +1,5 @@
 module AdminInfo
 	def add_admin_info
-		User.create!( name: "admin", address: "vatakara", email: "admin@admin.com", phone: "8891384095", state: "kerala", country: "India", password: "admin", activated: true, role: true)
+		User.create!( name: "admin", address: "vatakara", email: "admin@admin.com", phone: "8891384095", state: "kerala", country: "India", password: "admin", password_confirmation: "admin", activated: true, role: true)
 	end	
 end
