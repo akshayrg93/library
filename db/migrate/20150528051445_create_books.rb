8725d4e9fbf565f55430086c0ac0ec53635da136
akshayrg93 <akshay@qburst.com>
2015-06-26 12:34:50 +0530

    Fixed error in migrations

--- db/migrate/20150528051445_create_books.rb
+++ db/migrate/20150528051445_create_books.rb
@@ -1,16 +1,5 @@
 class CreateBooks < ActiveRecord::Migration
   def change
-      create_table :users do |t|
-      t.string :name
-      t.string :address
-      t.string :email
-      t.string :phone
-      t.string :state
-      t.string :country
-      
-      t.timestamps null: false
-    end
-
     create_table :books do |t|
       t.belongs_to :user, index: true
       t.string :name
