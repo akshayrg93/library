3f636a6225be27c1f53105c184f1eade62ff553a
akshayrg93 <akshay@qburst.com>
2015-07-02 17:24:05 +0530

    Included option for displaying home page of logged in users while opening a new tab

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,28 @@ class HomeController < ApplicationController
   layout "home"
   
   before_filter :require_admin, only: [:admin_home]   
+  before_filter :page_decider, except: [:admin_home]
   
-  def admin_home		
+  private
+  def page_decider
+  	if current_user.present?
+ 			if admin_user?
+ 				redirect_to admin_home_path
+ 			else
+ 				redirect_to user_path(current_user)
+ 			end
+  	else 
+    	render 'index'
+  	end
+	end
+	
+	def admin_user?
+  	current_user.present? && current_user.role?
+	end
+
+  def index	
+  end
+  
+  def admin_home 	
   end
 end
